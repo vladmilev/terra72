71d6863ce551556b06181374d2a7012d4ad263c5
Vladislav Milevsky <vlad_milev@mail.ru>
2022-06-10 04:20:28 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+#      version = "= 0.73"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "AQ..w0"
+  cloud_id  = "b1g43hks12bv8as0q653"
+  folder_id = "b1ga7u7bcjd3rfctvh7j"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_image" "ubuntu-image" {
+  name   = "my-ubuntu-image"
+  family = "ubuntu-2004-lts"
+}
