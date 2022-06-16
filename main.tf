aec2333de51d317a2a0b148d7e0be34641f1f966
vladmilev <vlad_milev@mail.ru>
2022-06-16 15:22:42 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "yandex" {
   token     = "AQ..w0"
   cloud_id  = "b1g43hks12bv8as0q653"
   folder_id = "b1ga7u7bcjd3rfctvh7j"
-  zone      = "ru-central1-b"
+  zone      = "ru-central1-c"
 }
 
 resource "yandex_compute_image" "ubuntu-image" {
